68b8530126d2ffc37961fb0e2bb42d1a66b69dec
Daniel Weinmann <danielweinmann@gmail.com>
2018-06-10 18:53:33 -0300

    Enable custom paths

--- src/actions/action.js
+++ src/actions/action.js
@@ -2,11 +2,12 @@ import apiAction from '../apiAction'
 
 export default options => {
   const { name, prefix, id, parseResponse, defaultParse, customParse } = options
+  const { path } = options
 
   return apiAction({
     ...options,
     prefix: `@${name}/${prefix}`,
-    path: `/${name}${id ? `/${id}` : ''}`,
+    path: `${path || `/${name}`}${id ? `/${id}` : ''}`,
     parseResponse: customParse || parseResponse || defaultParse,
   })
 }
