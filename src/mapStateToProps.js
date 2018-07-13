61c8b7950c3784322ae9430f9f9777abcfd95acb
Daniel Weinmann <danielweinmann@gmail.com>
2018-07-13 07:57:30 -0300

    Allow multiple levels on reducers

--- src/mapStateToProps.js
+++ src/mapStateToProps.js
@@ -1,1 +1,5 @@
-export default (state, { name }) => ({ ...state[name] })
+import split from 'lodash/split'
+import reduce from 'lodash/reduce'
+
+export default (state, { name }) =>
+  reduce(split(name, '.'), (object, key) => object[key], state)
