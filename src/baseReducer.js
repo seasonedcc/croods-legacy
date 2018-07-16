8c37533fd2328fb0139957863be7734117545f05
felipefreitag <ffvargas@gmail.com>
2018-07-16 12:48:53 -0300

    Refactor reducer

--- src/baseReducer.js
+++ src/baseReducer.js
@@ -1,25 +1,15 @@
-import { suffix } from './prefixedReducer'
+import identity from 'lodash/identity'
 
 import initialState from './reducer/initialState'
+import { suffix } from './prefixedReducer'
 
-export default (state = initialState, action = {}) => {
-  switch (suffix(action.type)) {
-    case 'SET_INFO': {
-      return { ...state, info: action.info }
-    }
-    case 'RESET_CREATED': {
-      return { ...state, created: null }
-    }
-    case 'RESET_CREATE_ERROR': {
-      return { ...state, createError: null }
-    }
-    case 'RESET_UPDATED': {
-      return { ...state, updated: null }
-    }
-    case 'RESET_DESTROYED': {
-      return { ...state, destroyed: null }
-    }
-    default:
-      return state
-  }
+const reducers = {
+  SET_INFO: (state, action) => ({ ...state, info: action.info }),
+  RESET_CREATED: (state, action) => ({ ...state, created: null }),
+  RESET_CREATE_ERROR: (state, action) => ({ ...state, createError: null }),
+  RESET_UPDATED: (state, action) => ({ ...state, updated: null }),
+  RESET_DESTROYED: (state, action) => ({ ...state, destroyed: null }),
 }
+
+export default (state = initialState, action = {}) =>
+  (reducers[suffix(action.type)] || identity)(state, action)
