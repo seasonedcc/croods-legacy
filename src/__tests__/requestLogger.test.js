82dcdbfdffb332ea7378613ccddcfe085ee34f72
Gustavo Guichard <gustavoguichard@gmail.com>
2018-08-30 16:32:45 -0300

    Fix lint

--- src/__tests__/requestLogger.test.js
+++ src/__tests__/requestLogger.test.js
@@ -5,9 +5,12 @@ it('calls console.group and logs the passed parameters', () => {
 
   requestLogger('http://foo.com', { method: 'get', foo: 'bar' })
 
-  expect(console.group).toHaveBeenCalledWith('%cREQUEST: ', 'color: mediumpurple;')
+  expect(console.group).toHaveBeenCalledWith(
+    '%cREQUEST: ',
+    'color: mediumpurple;',
+  )
   expect(console.log).toHaveBeenCalledTimes(2)
-  expect(console.log).toHaveBeenCalledWith({'foo': 'bar'})
+  expect(console.log).toHaveBeenCalledWith({ foo: 'bar' })
   expect(console.log).toHaveBeenCalledWith('GET: http://foo.com')
   expect(console.groupEnd).toHaveBeenCalledWith()
 })
