2557cb67f048ec8299e3acf437e9f4dbdcb3b463
Giovani de Sousa Leite <sousa_giovani@hotmail.com>
2019-01-21 14:59:17 -0200

    Solve conflicts

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,9 +1,7 @@
 export const imports = {
   'src/docs/GettingStarted.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-getting-started" */ 'src/docs/GettingStarted.mdx'),
-<<<<<<< HEAD
   'src/docs/New.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-new" */ 'src/docs/New.mdx'),
-=======
->>>>>>> docz
 }
+s
