aa9f9bbd2b3ba6621f514298cbf7eebaf978f29a
Giovani de Sousa Leite <sousa_giovani@hotmail.com>
2019-01-21 16:44:58 -0200

    Add Info Documentation

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,4 +1,6 @@
 export const imports = {
   'src/docs/GettingStarted.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-docs-getting-started" */ 'src/docs/GettingStarted.mdx'),
+  'src/docs/Info.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-info" */ 'src/docs/Info.mdx'),
 }
