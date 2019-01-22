91305166e5c23a3df5884e7bf64d4f43959371c4
Giovani de Sousa Leite <sousa_giovani@hotmail.com>
2019-01-22 11:14:27 -0200

    gitignore docz files

--- .docz/app/imports.js
+++
@@ -1,4 +0,0 @@
-export const imports = {
-  'src/docs/GettingStarted.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-docs-getting-started" */ 'src/docs/GettingStarted.mdx'),
-}
